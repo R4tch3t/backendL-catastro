2404ac0bec0a62dd584811e0d258d67dad95045f
R4tch3t <bebetovic@hotmail.com>
2021-03-17 10:57:56 -0600

    u

--- src/functions/padrones.js
+++ src/functions/padrones.js
@@ -84,7 +84,9 @@ const _padrones = (req, res) => {
                                                 if (!err) {
                                                     if (result.length > 0) {
                                                         outJSON.orden = result[0]
-                                                        outJSON.orden.dateUpL = outJSON.orden.dateUp.toLocaleString();
+                                                        const dO = new Date(outJSON.orden.dateUp)
+                                                        dO.setHours(dO.getHours()-1) 
+                                                        outJSON.orden.dateUpL = dO.toLocaleString();
                                                         if (outJSON.orden.zona === "0" || outJSON.orden.zona === "") {
                                                             outJSON.orden.zona = getZone(outJSON.orden.colonia)
                                                         }
